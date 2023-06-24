3e2ae3b17fcaa4c7d2933a97dcc638d84444d04b
boomchanotai <boom9387@hotmail.com>
2023-06-25 04:23:46 +0700

    feat: shake
    Co-authored-by: ImSoZRious <ImSoZRious@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,49 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import createScreenShake from "screen-shake";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window.DeviceMotionEvent != "undefined") {
+      // Shake sensitivity (a lower number is more)
+      var sensitivity = 20;
+
+      // Position variables
+      var x1 = 0,
+        y1 = 0,
+        z1 = 0,
+        x2 = 0,
+        y2 = 0,
+        z2 = 0;
+
+      // Listen to motion events and update the position
+      window.addEventListener(
+        "devicemotion",
+        function (e: any) {
+          x1 = e.accelerationIncludingGravity.x;
+          y1 = e.accelerationIncludingGravity.y;
+          z1 = e.accelerationIncludingGravity.z;
+        },
+        false
+      );
+
+      // Periodically check the position and fire
+      // if the change is greater than the sensitivity
+      setInterval(function () {
+        var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
+
+        if (change > sensitivity) {
+          alert("Shake!");
+        }
+
+        // Update new position
+        x2 = x1;
+        y2 = y1;
+        z2 = z1;
+      }, 150);
+    }
+  }, []);
+
   return (
     <>
       <Head>
