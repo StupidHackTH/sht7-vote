357abd766a858ad24a5e6d167aa96aa5651dc460
boomchanotai <boom9387@hotmail.com>
2023-06-25 04:45:55 +0700

    feat: shake

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,25 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+  const [key, setKey] = useState("");
   useEffect(() => {
     if (typeof window.DeviceMotionEvent != "undefined") {
-      // Shake sensitivity (a lower number is more)
-      var sensitivity = 20;
-
-      // Position variables
-      var x1 = 0,
-        y1 = 0,
-        z1 = 0,
-        x2 = 0,
-        y2 = 0,
-        z2 = 0;
-
-      // Listen to motion events and update the position
-      window.addEventListener(
-        "devicemotion",
-        function (e: any) {
-          x1 = e.accelerationIncludingGravity.x;
-          y1 = e.accelerationIncludingGravity.y;
-          z1 = e.accelerationIncludingGravity.z;
-        },
-        false
-      );
-
-      // Periodically check the position and fire
-      // if the change is greater than the sensitivity
-      setInterval(function () {
-        var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
-
-        if (change > sensitivity) {
-          alert("Shake!");
-        }
-
-        // Update new position
-        x2 = x1;
-        y2 = y1;
-        z2 = z1;
-      }, 150);
+      window.addEventListener("devicemotion", handleMotionEvent, true);
     }
   }, []);
+
+  const handleMotionEvent = (event: any) => {
+    const x = event.accelerationIncludingGravity.x;
+    const y = event.accelerationIncludingGravity.y;
+    const z = event.accelerationIncludingGravity.z;
+
+    const key = `${x},${y},${z}`;
+
+    setKey(key);
+    setCount((prev) => prev + 1);
+  };
 
   return (
     <>
@@ -51,7 +29,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className="h-[200vh]">
+        {count}: {key}
+      </main>
     </>
   );
 }
