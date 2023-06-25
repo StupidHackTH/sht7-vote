df14eafa17368cc6924d15079324776119740a06
boomchanotai <boom9387@hotmail.com>
2023-06-25 14:10:34 +0700

    feat: shake

--- components/Vote.tsx
+++ components/Vote.tsx
@@ -1,5 +1,6 @@
 import { app } from "@/utils/firebase";
-import { collectTime, tickTime } from "@/utils/timeCounter";
+import { getMobileOperatingSystem } from "@/utils/getMobileOperatingSystem";
+import { collectTime, resetTime, tickTime } from "@/utils/timeCounter";
 import { doc, getFirestore, onSnapshot, setDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
@@ -23,13 +24,13 @@ const Vote = ({ userId }: { userId: string }) => {
       motion1.x - motion2.x + motion1.y - motion2.x + motion1.z - motion2.x
     );
 
-    if (change > 100) {
+    if (change > 80) {
       setTimeout(() => {
         if (currentTeam.id !== "end") {
           setCount(count + 1);
           tickTime();
         }
-      }, 300);
+      }, 280);
     }
 
     // Update new position
@@ -41,24 +42,35 @@ const Vote = ({ userId }: { userId: string }) => {
   }, [motion1, motion2]);
 
   const handleRequestMotion = async () => {
-    if (typeof (DeviceMotionEvent as any).requestPermission === "function") {
-      (DeviceMotionEvent as any)
-        .requestPermission()
-        .then((permissionState: any) => {
-          if (permissionState === "granted") {
-            window.addEventListener("devicemotion", (e: any) => {
-              setMotion1({
-                x: e.accelerationIncludingGravity.x,
-                y: e.accelerationIncludingGravity.y,
-                z: e.accelerationIncludingGravity.z,
+    const mobile = getMobileOperatingSystem();
+    if (mobile === "iOS") {
+      if (typeof (DeviceMotionEvent as any).requestPermission === "function") {
+        (DeviceMotionEvent as any)
+          .requestPermission()
+          .then((permissionState: any) => {
+            if (permissionState === "granted") {
+              window.addEventListener("devicemotion", (e: any) => {
+                setMotion1({
+                  x: e.accelerationIncludingGravity.x,
+                  y: e.accelerationIncludingGravity.y,
+                  z: e.accelerationIncludingGravity.z,
+                });
               });
-            });
-          }
-        })
-        .catch(console.error);
+            }
+          })
+          .catch(console.error);
+      } else {
+        // handle regular non iOS 13+ devices
+        console.log("Not Supported");
+      }
     } else {
-      // handle regular non iOS 13+ devices
-      console.log("Not Supported");
+      window.addEventListener("devicemotion", (e: any) => {
+        setMotion1({
+          x: e.accelerationIncludingGravity.x,
+          y: e.accelerationIncludingGravity.y,
+          z: e.accelerationIncludingGravity.z,
+        });
+      });
     }
   };
 
@@ -95,25 +107,20 @@ const Vote = ({ userId }: { userId: string }) => {
     if (count == 0) return;
 
     if (currentTeam.id !== "end") {
-      console.log(currentTeam.id, count);
+      const time = collectTime();
+      console.log(currentTeam.id, count, time);
       setDoc(
         doc(db, "results", userId),
         {
-          [currentTeam.id]: count,
+          [currentTeam.id]: { count, time },
         },
         {
           merge: true,
         }
       );
-      setDoc(
-        doc(db, "time", userId),
-        {
-          [currentTeam.id]: collectTime(),
-        },
-        {
-          merge: true,
-        }
-      );
+
+      setCount(0);
+      resetTime();
     }
   }, [currentTeam.id, db]);
 
