282e1456a7f16c60b3bd932578d0f645969410f3
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-25 14:41:19 +0700

    bruh

--- components/Personal.tsx
+++ components/Personal.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 
 const Personal = ({ session }: { session: any }) => {
   return (
     <div className="bg-gray-800 text-white px-8 py-4 flex flex-row justify-start items-center space-x-4">
       <div className="relative w-16 h-16">
-        <Image
+        <img
           src={session.user.image}
           alt=""
           className="rounded-lg object-cover"
